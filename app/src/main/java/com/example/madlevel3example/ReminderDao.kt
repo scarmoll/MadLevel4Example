7d25b18ddc0d3a125ad87a434df6626c487da506
Oscar Mollé <scarmoll@gmail.com>
2020-10-04 12:16:58 +0200

    work pls

--- app/src/main/java/com/example/madlevel3example/ReminderDao.kt
+++ app/src/main/java/com/example/madlevel3example/ReminderDao.kt
@@ -1,7 +1,6 @@
 package com.example.madlevel3example
 
 import androidx.room.*
-import com.example.madlevel2example.Reminder
 
 @Dao
 interface ReminderDao {
--- app/src/main/java/com/example/madlevel3example/RemindersFragment.kt
+++ app/src/main/java/com/example/madlevel3example/RemindersFragment.kt
@@ -13,8 +13,6 @@ import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import com.example.madlevel2example.Reminder
-import com.example.madlevel2example.ReminderAdapter
 import kotlinx.android.synthetic.main.fragment_reminders.*
 
 /**
